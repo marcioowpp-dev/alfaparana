99d8e08ef62ba369c166c92e4ac4c1cd9ae1c744
marcioowpp-dev <marcioowpp@gmail.com>
2026-02-10 20:10:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-// Efeito Reveal ao rolar a página
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
+        var elementVisible = 100;
         if (elementTop < windowHeight - elementVisible) {
             reveals[i].classList.add("active");
         }
     }
 }
 window.addEventListener("scroll", reveal);
-reveal(); // Executa ao carregar para mostrar o topo
+reveal(); // Iniciar no carregamento
